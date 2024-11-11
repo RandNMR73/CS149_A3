ca05bccca4250b7b20a64c865fca0ae89dc3c507
Sai Konkimalla <sai.konkimalla@gmail.com>
2024-11-11 04:15:54 -0800

    only using slower circle box test

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -466,7 +466,7 @@ __global__ void kernelRenderCircles2(int tileSize, int totalTiles, int tilesPerX
             float3 p = *(float3*)(&cuConstRendererParams.position[index3]);
             float  rad = cuConstRendererParams.radius[index];
 
-            int flag = circleInBoxConservative(p.x, p.y, rad, boxL, boxR, boxT, boxB);
+            int flag = circleInBox(p.x, p.y, rad, boxL, boxR, boxT, boxB);
             // if (flag == 1) {
             //     flag = circleInBox(p.x, p.y, rad, boxL, boxR, boxT, boxB);
             // }
